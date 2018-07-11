691e1aa687be05e9a82fc571acfbc95b444343dc
Sebastian <sebastianlealj10@gmail.com>
2018-07-10 21:33:33 -0500

    sdas

--- Monitoring.sh
+++ Monitoring.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-URL="https://welcome.sirus.su/#/page-8"
-mv new.html old.html 2> /dev/null
+URL="http://localhost/Phishingtest.html"
 curl $URL -L --compressed -s > new.html
-for (( ; ; )); 
+mv new.html old.html 2> /dev/null
+B_len=$(cat old.html | wc -l)
+echo 'longitud del sitio original: '$B_len
+for (( ; ; ));
 do
-    mv new.html old.html 2> /dev/null
     curl $URL -L --compressed -s > new.html
     A_len=$(cat new.html | wc -l)
-    echo $A_len
-    B_len=$(cat old.html | wc -l)
-    echo $B_len 
-    diff=$(sdiff -B -s new.html old.html | wc -l)
-    echo $diff
-    common=$(expr $A_len - $diff)
-    echo $common
-    percentage=$(echo "100 * $common / $B_len" | bc)
-    echo $percentage
+    if [ $A_len -gt 0 ]; then
+      echo 'longitud del nuevo sitio: '$A_len
+      diff=$(sdiff -B -s new.html old.html | wc -l)
+      echo 'Diferencia: ' $diff
+      common=$(expr $A_len - $B_len)
+      echo 'cambio: '$common
+      percentage=$(echo "100 * $diff/ $A_len" | bc)
+    else
+      percentage=0
+    fi
+    echo 'Porcentaje de diferencia: ' $percentage
+    if [[ $percentage -gt 40 ]]; then
+      echo 'Posible Reactivacion!!'
+    fi
     
     sleep 10
 done
--- Percentage.sh
+++ Percentage.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
     A_len=$(cat new.html | wc -l)
-    echo $A_len
+    echo 'longitud del nuevo sitio: '$A_len
     B_len=$(cat old.html | wc -l)
-    echo $B_len 
+    echo 'longitud del sitio original: '$B_len
     diff=$(sdiff -B -s new.html old.html | wc -l)
-    echo $diff
-    common=$(expr $A_len - $diff)
-    echo $common
-    percentage=$(echo "100 * $common / $B_len" | bc)
-    echo $percentage
-    sleep 10
-
+    echo 'Diferencia: ' $diff
+    common=$(expr $A_len - $B_len)
+    echo 'resta: ' $diff
+    percentage=$(echo "100 * $diff/ $A_len" | bc)
+    echo 'Porcentaje de diferencia: ' $percentage
