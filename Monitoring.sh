81707b77d193a1d9e8c616a5b137024677efb313
Sebastian <sebastianlealj10@gmail.com>
2018-07-16 21:48:31 -0500

    90%

--- Monitoring.sh
+++ Monitoring.sh
@@ -11,13 +11,13 @@ set_proxy () {
       echo "Brasil"
       ;;
     Chile)
-      curl $2 -L --compressed -s -x, --proxy http://customer-analyst-cc-CL:CTAC%40cyxtera.com2018@pr.oxylabs.io:7777> $3
+      curl $2 -x, --proxy http://customer-analyst-cc-CL:CTAC%40cyxtera.com2018@pr.oxylabs.io:7777> $3
       ;;
     Mexico)
-      curl $2 -L --compressed -s -x, --proxy http://customer-analyst-cc-MX:CTAC%40cyxtera.com2018@pr.oxylabs.io:7777> $3
+      curl $2 -x, --proxy http://customer-analyst-cc-MX:CTAC%40cyxtera.com2018@pr.oxylabs.io:7777> $3
       ;;
     Japan)
-      curl $2 -L --compressed -s -x, --proxy http://customer-analyst-cc-JP:CTAC%40cyxtera.com2018@pr.oxylabs.io:7777> $3
+      curl $2 -x, --proxy http://customer-analyst-cc-JP:CTAC%40cyxtera.com2018@pr.oxylabs.io:7777> $3
       ;;
     *)
       curl $2 > $3
@@ -25,18 +25,19 @@ set_proxy () {
 esac
 }
 
-rm Reactivated.csv
+rm  ChangeStatus.csv
 if [ ! -d HTMLS ]
 then
     mkdir -p HTMLS
 fi
 echo TicketID,Country,URL > tickets.csv
-echo -e "easy1,USA,https://www.computerhope.com" >> tickets.csv
-echo -e "easy2,COLOMBIA,https://stackoverflow.com" >> tickets.csv
-echo -e "test,Brazil,http://americanas.revolucaolanches.com.br/j7_americanas/home.php?170239.189.6.21.225.80728161847219/" >> tickets.csv
-echo -e "testjapanproxy,Japan,https://mufg-ufj-nicos-group.charlesoliverblog.com/index_pc.php" >> tickets.csv
 echo -e "localtest,Colombia,http://localhost/Phishingtest.html" >> tickets.csv
-echo -e "testjapanproxy2,Japan,https://mitsubishi-ufj-financial.findmyincomegap.com/index_pc.php" >> tickets.csv
+echo -e "test,Brazil,https://www93.osmaisdesejadosdacategoria.com/tkn870029/smartphone-samsung-galaxy-s8-dual-chip-android-7-0-tela-5-8-octa-core-2-3ghz-64gb-4g-camera-12mp-preto/132118431/pr" >> tickets.csv
+echo -e "test2,Mexico,https://noticiasadicionaisbrlltda.com/web/desktop/home.php?cli=&/rM2SrorGcM/1c2Ej9WdaZ.php" >> tickets.csv
+echo -e "test3,Brazil,http://fanismastrokostas.gr/management/www.banco.bradesco/html/classic/shtm/" >> tickets.csv
+echo -e "test4,Brazil,https://cef.gov-br.app/" >> tickets.csv
+echo -e "test5,Japan,https://mufg-ufj-nicos-group.charlesoliverblog.com/index_pc.php" >> tickets.csv
+echo -e "test6,Colombia,https://www.androidapkdescargar.com/banbif-app/pe.com.banBifBanking.icBanking.androidUI/" >> tickets.csv
 
 while read line
 do
@@ -55,26 +56,41 @@ do
       echo 'longitud del sitio original: '$oldsite_len
       set_proxy $COUNTRY $URL "newsite.html"
       newsite_len=$(cat newsite.html | wc -l)
-      if [ $newsite_len -gt 0 ];
+      if [ $newsite_len -ge $oldsite_len ];
         then
           echo 'longitud del nuevo sitio: '$newsite_len
-          diff=$(sdiff -B -s newsite.html "HTMLS/$ID".html | wc -l)
-          echo 'Diferencia: ' $diff
-          common=$(expr $newsite_len - $oldsite_len)
-          echo 'cambio: '$common
-          percentage=$(echo "100 * $diff/ $newsite_len" | bc)
-      else
-        percentage=0
+          if [ $newsite_len != 0 ];
+            then
+              diff=$(sdiff -B -s newsite.html "HTMLS/$ID".html | wc -l)
+              echo 'Diferencia: ' $diff
+              common=$(expr $newsite_len - $oldsite_len)
+              echo 'cambio: '$common
+              percentage=$(echo "100 * $diff/ $newsite_len" | bc)
+            else
+              percentage=0
+          fi
+        else
+          echo 'longitud del nuevo sitio: '$newsite_len
+            if [ $newsite_len != 0 ];
+              then
+                diff=$(sdiff -B -s "HTMLS/$ID".html  newsite.html | wc -l)
+                echo 'Diferencia: ' $diff
+                common=$(expr $oldsite_len - $newsite_len)
+                echo 'cambio: '$common
+                percentage=$(echo "100 * $diff/ $oldsite_len" | bc)
+            else
+              percentage=100
+            fi
       fi
       echo 'Porcentaje de diferencia: ' $percentage
       if [[ $percentage -gt 40 ]];
         then
           echo "Reactivated"
-          echo TicketID,Country,URL > Reactivated.csv
-          echo -e $ID,$COUNTRY,$URL >> Reactivated.csv
+          echo TicketID,Country,URL > ChangeStatus.csv
+          echo -e $ID,$COUNTRY,$URL >> ChangeStatus.csv
       fi
   fi
 done <tickets.csv
-#  curl http://americanas.revolucaolanches.com.br/j7_americanas/home.php?170239.189.6.21.225.80728161847219/ -x, --proxy http://customer-analyst-cc-BR:CTAC%40cyxtera.com2018@pr.oxylabs.io:7777> proxytest.html
+#  curl https://www35.osmaisdesejadosdacategoria.com/tkn3885063/smartphone-samsung-galaxy-s8-dual-chip-android-7-0-tela-5-8-octa-core-2-3ghz-64gb-4g-camera-12mp-preto/132118431/pr -x, --proxy http://customer-analyst-cc-BR:CTAC%40cyxtera.com2018@pr.oxylabs.io:7777> proxytest.html
 
 #curl -x http://pr.oxylabs.io:7777 -- customer-analyst-cc-BR: CTAC@cyxtera.com2018 -L http://google.com
