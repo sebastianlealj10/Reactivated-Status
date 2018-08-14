0ec6c1f6606de3ad944891b7c0bff6cee6d1202e
sebastianlealj10 <sebastianlealj10@gmail.com>
2018-08-13 21:31:26 -0500

    Update Monitoring.sh

--- Monitoring.sh
+++ Monitoring.sh
@@ -3,43 +3,43 @@
 set_proxy () {
   case $1 in
     USA)
-      curl $2 -# -k -L -w -x,--progress-bar --insecure --write-out "\n[%{http_code}]\n[%{remote_ip}]" \
+      curl $2 -# -k -L -w -x,--progress-bar --insecure --write-out "\n[%{http_code}]" \
       --proxy http://customer-analyst-cc-US:CTAC%40cyxtera.com2018@pr.oxylabs.io:7777> $3
       ;;
     BRAZIL)
-      curl $2 -# -k -L -w -x,--progress-bar --insecure --write-out "\n[%{http_code}]\n[%{remote_ip}]" \
+      curl $2 -# -k -L -w -x,--progress-bar --insecure --write-out "\n[%{http_code}]" \
       --proxy http://customer-analyst-cc-BR:CTAC%40cyxtera.com2018@pr.oxylabs.io:7777> $3
       ;;
     CHILE)
-      curl $2 -# -k -L -w -x,--progress-bar --insecure --write-out "\n[%{http_code}]\n[%{remote_ip}]" \
+      curl $2 -# -k -L -w -x,--progress-bar --insecure --write-out "\n[%{http_code}]" \
       --proxy http://customer-analyst-cc-CL:CTAC%40cyxtera.com2018@pr.oxylabs.io:7777> $3
       ;;
     MEXICO)
-      curl $2 -# -k -L -w -x,--progress-bar --insecure --write-out "\n[%{http_code}]\n[%{remote_ip}]" \
+      curl $2 -# -k -L -w -x,--progress-bar --insecure --write-out "\n[%{http_code}]" \
       --proxy http://customer-analyst-cc-MX:CTAC%40cyxtera.com2018@pr.oxylabs.io:7777> $3
       ;;
     JAPAN)
-      curl $2 -# -k -L -w -x,--progress-bar --insecure --write-out "\n[%{http_code}]\n[%{remote_ip}]" \
+      curl $2 -# -k -L -w -x,--progress-bar --insecure --write-out "\n[%{http_code}]" \
       --proxy http://customer-analyst-cc-JP:CTAC%40cyxtera.com2018@pr.oxylabs.io:7777> $3
       ;;
     PERU)
-      curl $2 -# -L -w -x,--progress-bar --insecure --write-out "\n[%{http_code}]\n[%{remote_ip}]" \
+      curl $2 -# -L -w -x,--progress-bar --insecure --write-out "\n[%{http_code}]" \
       --proxy http://customer-analyst-cc-PE:CTAC%40cyxtera.com2018@pr.oxylabs.io:7777> $3
       ;;
     ARGENTINA)
-      curl $2 -# -k -L -w -x,--progress-bar --insecure --write-out "\n[%{http_code}]\n[%{remote_ip}]" \
+      curl $2 -# -k -L -w -x,--progress-bar --insecure --write-out "\n[%{http_code}]" \
       --proxy http://customer-analyst-cc-AR:CTAC%40cyxtera.com2018@pr.oxylabs.io:7777> $3
       ;;
     BOLIVIA)
-      curl $2 -# -k -L -w -x,--progress-bar --insecure --write-out "\n[%{http_code}]\n[%{remote_ip}]" \
+      curl $2 -# -k -L -w -x,--progress-bar --insecure --write-out "\n[%{http_code}]" \
       --proxy http://customer-analyst-cc-BO:CTAC%40cyxtera.com2018@pr.oxylabs.io:7777> $3
       ;;
     DOMINICANREPUBLIC)
-      curl $2 -# -k -L -w -x,--progress-bar --insecure --write-out "\n[%{http_code}]\n[%{remote_ip}]" \
+      curl $2 -# -k -L -w -x,--progress-bar --insecure --write-out "\n[%{http_code}]" \
       --proxy http://customer-analyst-cc-DO:CTAC%40cyxtera.com2018@pr.oxylabs.io:7777> $3
       ;;
     *)
-      curl $2 -# -k -L -w,--progress-bar --insecure --write-out "\n[%{http_code}]\n[%{remote_ip}]"> $3
+      curl $2 -# -k -L -w,--progress-bar --insecure --write-out "\n[%{http_code}]"> $3
       ;;
   esac
 }
@@ -55,11 +55,11 @@ then
 fi
 #..............................................Example of csv
 echo TicketID,Country,URL > tickets.csv
-echo -e "localtest1,Colombia,http://localhost/Phishingtest.html" >> tickets.csv
-echo -e "localtest2,Colombia,http://localhost/Phishingtest2.html" >> tickets.csv
 echo -e "test2,JAPAN,http://www.mufg-jp.biz/inet/life/ninsyou/entry/top" >> tickets.csv
+echo -e "test3,MEXICO,http://myrunnginesz.co.uk/wp-bnorte.php" >> tickets.csv
 
-#..............................This loop is used for read the tickets csv file
+#..............................This loop is used for read the tickets csv file  curl -L https://stackoverflow.com | grep '<title>'
+
 while read line
 do
 #....................................
@@ -75,20 +75,40 @@ do
         then
             var="HTMLS/$ID.html"
             set_proxy $COUNTRY $URL $var
-            resp_old=$( tail -2 "HTMLS/$ID".html | head -1 | sed 's/.$//' | sed 's/^.//' )
+            resp_old=$( tail -1 "HTMLS/$ID".html | sed 's/.$//' | sed 's/^.//' )
+            echo $?
             if [ "$?" -ne 0 ] || [ "$resp_old" -eq 000 ]
               then
                 set_proxy $COUNTRY $URL $var
-                resp_old=$( tail -2 "HTMLS/$ID".html | head -1 | sed 's/.$//' | sed 's/^.//' )
-            fi
-            if [ "$?" -ne 0 ] || [ "$resp_old" -eq 000 ]
-              then
-                set_proxy $COUNTRY $URL $var
-                resp_old=$( tail -2 "HTMLS/$ID".html | head -1 | sed 's/.$//' | sed 's/^.//' )
-            fi
-            if [ "$?" -ne 0 ] || [ "$resp_old" -eq 000 ]
-              then
-                rm "HTMLS/$ID.html"
+                resp_old=$( tail -1 "HTMLS/$ID".html | sed 's/.$//' | sed 's/^.//' )
+                ip_old=$(dig +short $(echo $URL | awk -F[/:] '{print $4}'))
+                echo $ip_old
+                echo -e $ip_old >> "HTMLS/$ID".html
+                if [ "$?" -ne 0 ] || [ "$resp_old" -eq 000 ]
+                  then
+                    set_proxy $COUNTRY $URL $var
+                    resp_old=$( tail -1 "HTMLS/$ID".html | sed 's/.$//' | sed 's/^.//' )
+                    if [ "$?" -ne 0 ] || [ "$resp_old" -eq 000 ]
+                      then
+                        rm "HTMLS/$ID.html"
+                    fi
+                  else
+                    resp_old=$( tail -1 "HTMLS/$ID".html | sed 's/.$//' | sed 's/^.//' )
+                    ip_old=$(dig +short $(echo $URL | awk -F[/:] '{print $4}'))
+                    title=$(cat "HTMLS/$ID".html  | grep '<title>' | sed -n 's/.*<title>\(.*\)<\/title>.*/\1/ip;T;q')
+                    echo $ip_old
+                    echo $title
+                    echo -e "\n[$ip_old]" >> "HTMLS/$ID".html
+                    echo -e "[$title]" >> "HTMLS/$ID".html
+                fi
+              else
+                resp_old=$( tail -1 "HTMLS/$ID".html | sed 's/.$//' | sed 's/^.//' )
+                ip_old=$(dig +short $(echo $URL | awk -F[/:] '{print $4}'))
+                title=$(cat "HTMLS/$ID".html  | grep '<title>' | sed -n 's/.*<title>\(.*\)<\/title>.*/\1/ip;T;q')
+                echo $ip_old
+                echo $title
+                echo -e "\n[$ip_old]" >> "HTMLS/$ID".html
+                echo -e "[$title]" >> "HTMLS/$ID".html
             fi
       continue
       fi
@@ -98,24 +118,26 @@ if [ -f "HTMLS/$ID.html" ]
     then
 #..........................
 #.....................extracts some data from the html
-      resp_old=$( tail -2 "HTMLS/$ID".html | head -1 | sed 's/.$//' | sed 's/^.//' )
-      ip_old=$( tail -1 "HTMLS/$ID".html | sed 's/.$//' | sed 's/^.//' )
+      resp_old=$( tail -3 "HTMLS/$ID".html | head -2 | sed 's/.$//' | sed 's/^.//' )
+      ip_old=$( tail -2 "HTMLS/$ID".html | head -1 | sed 's/.$//' | sed 's/^.//' )
+      title_old=$( tail -1 "HTMLS/$ID".html | sed 's/.$//' | sed 's/^.//' )
       echo "resp old: "$resp_old
       echo "ip old: "$ip_old
+      echo "title old: "$title_old
       first_old="${resp_old:0:1}"
       oldsite_len=$(cat "HTMLS/$ID".html | wc -l)
 #..................Load the html by proxy
       set_proxy $COUNTRY $URL "newsite.html"
-      resp_new=$( tail -2 newsite.html | head -1 | sed 's/.$//' | sed 's/^.//' )
+      resp_new=$( tail -1 newsite.html | sed 's/.$//' | sed 's/^.//' )
       if [ "$?" -ne 0 ] || [ "$resp_new" -eq 000 ]
         then
           set_proxy $COUNTRY $URL "newsite.html"
-          resp_new=$( tail -2 newsite.html | head -1 | sed 's/.$//' | sed 's/^.//' )
+          resp_new=$( tail -1 newsite.html | sed 's/.$//' | sed 's/^.//' )
       fi
       if [ "$?" -ne 0 ] || [ "$resp_new" -eq 000 ]
         then
           set_proxy $COUNTRY $URL "newsite.html"
-          resp_new=$( tail -2 newsite.html | head -1 | sed 's/.$//' | sed 's/^.//' )
+          resp_new=$( tail -1 newsite.html | sed 's/.$//' | sed 's/^.//' )
       fi
       if [ "$?" -ne 0 ] || [ "$resp_new" -eq 000 ]
         then
@@ -124,8 +146,8 @@ if [ -f "HTMLS/$ID.html" ]
 #..............................................
 #....................extracts some data from the current html
       newsite_len=$(cat newsite.html | wc -l)
-      resp_new=$( tail -2 newsite.html | head -1 | sed 's/.$//' | sed 's/^.//' )
-      ip_new=$( tail -1 newsite.html | sed 's/.$//' | sed 's/^.//' )
+      resp_new=$( tail -1 newsite.html | sed 's/.$//' | sed 's/^.//' )
+      ip_new=$(dig +short $(echo $URL | awk -F[/:] '{print $4}'))
       echo "resp new: "$resp_new
       echo "ip new: "$ip_new
 #................................................
@@ -161,3 +183,4 @@ if [ -f "HTMLS/$ID.html" ]
   fi
 fi
 done <tickets.csv
+
